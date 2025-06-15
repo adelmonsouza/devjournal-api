81530e69f9f0f37e79eaf71a40b6df8c179c5260
Adelmon Souza <35411837+adelmonsouza@users.noreply.github.com>
2025-06-16 00:49:06 +0100

    Add files via upload

---
+++ DevJournalApiApplication.java
@@ -0,0 +1,13 @@
+package com.devjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJournalApiApplication.class, args);
+	}
+
+}
